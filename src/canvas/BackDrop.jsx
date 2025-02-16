de919727881c87ff74869c22e0df1165c4e65f3d
lufyDev <vishalpundhirofficial@gmail.com>
2025-02-16 14:56:43 +0530

    fixed backdrop bug

--- src/canvas/BackDrop.jsx
+++ src/canvas/BackDrop.jsx
@@ -1,34 +1,31 @@
-import React, {useRef} from 'react'
+import React, { useRef } from 'react'
 import { easing } from 'maath'
 import { useFrame } from '@react-three/fiber'
-import { AccumulativeShadows, RandomizedLight } from '@react-three/drei'
+import { AccumulativeShadows, RandomizedLight } from '@react-three/drei';
 
-const BackDrop = () => {
-  const shadows = useRef(null)
+const Backdrop = () => {
+  const shadows = useRef();
 
-  // useFrame((state, delta) => {
-  //   easing.dampC(cube.current.material.color, '#000', 0.25, delta)
-  // })
   return (
     <AccumulativeShadows
       ref={shadows}
       temporal
       frames={60}
-      alphaTest={0.85}
-      scale={10}
+      alphaTest={0.20}
+      scale={4}
       rotation={[Math.PI / 2, 0, 0]}
       position={[0, 0, -0.14]}
     >
       <RandomizedLight 
-        amount={4} 
-        radius={9}
+        amount={4}
+        radius={15}
         intensity={0.55}
         ambient={0.25}
         position={[5, 5, -10]}
       />
       <RandomizedLight 
-        amount={4} 
-        radius={5}
+        amount={4}
+        radius={10}
         intensity={0.25}
         ambient={0.55}
         position={[-5, 5, -9]}
@@ -37,4 +34,4 @@ const BackDrop = () => {
   )
 }
 
-export default BackDrop
+export default Backdrop
